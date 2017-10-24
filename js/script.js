3b125cd9df84370f63a068d6d96b7642ac473438
๖ۜG н σ ѕ т彡 <mig123@hotmail.ca>
2017-10-23 21:18:00 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,76 +1,21 @@
-
-const indexConversion = {
-	"0": [],
-	"1": [4],
-	"2": [3],
-	"3": [3, 4],
-	"4": [2],
-	"5": [2, 4],
-	"6": [2, 3],
-	"7": [2, 3, 4],
-	"8": [1],
-	"9": [1, 4]
-};
-
-let hr;
-let min;
-let sec;
-
-function updateDisplay() {
-	updateTime();
-	updateDOM();
+(function() {
+	window.onload = function() {
+const b_i = [[], [3], [2], [2, 3], [1], [1, 3], [1, 2], [1, 2, 3], [0], [0, 3]];
+function getTime() {
+	return (new Date()).toLocaleTimeString('en-CA', {hour12:false})
+	.split(/:| /).map(t => t.padStart(2,'0'));
 }
-
-function updateTime() {
-	const now = new Date();
-
-	let hours = now.getHours().toString();
-	let minutes = now.getMinutes().toString();
-	let seconds = now.getSeconds().toString();
-
-	hr = (hours.length == 1) ? `0${hours}` : hours;
-
-	min = (minutes.length == 1) ? `0${minutes}` : minutes;
-
-	sec = (seconds.length == 1) ? `0${seconds}` : seconds;
-}
-
 function updateDOM() {
-	//
-	let a;
-	//----------------------------------Reset Lights-------------------------------//
-	a = document.querySelectorAll('.one')
-	for (var i = 0, len = a.length; i < len; i++) { a[i].classList.remove('one') }
-	//----------------------------------------------------------------------------//
-	//
-	// | hourOne - First Digit Of hr |
-	a = indexConversion[hr[0]]
-	for (var i = 0, len = a.length; i < len; i++) { document.querySelector(`.hourOne div:nth-child(${a[i]})`).classList.add('one') }
-	//
-	// | hourTwo - Second Digit Of hr |
-	a = indexConversion[hr[1]]
-	for (var i = 0, len = a.length; i < len; i++) { document.querySelector(`.hourTwo div:nth-child(${a[i]})`).classList.add('one') }
-	//
-	// | minOne - First Digit Of min |
-	a = indexConversion[min[0]]
-	for (var i = 0, len = a.length; i < len; i++) { document.querySelector(`.minOne div:nth-child(${a[i]})`).classList.add('one') }
-	//
-	// | minTwo - Second Digit Of min |
-	a = indexConversion[min[1]]
-	for (var i = 0, len = a.length; i < len; i++) { document.querySelector(`.minTwo div:nth-child(${a[i]})`).classList.add('one') }
-	//
-	// | secOne - First Digit Of sec |
-	a = indexConversion[sec[0]]
-	for (var i = 0, len = a.length; i < len; i++) { document.querySelector(`.secOne div:nth-child(${a[i]})`).classList.add('one') }
-	//
-	// | secTwo - Second Digit Of sec |
-	a = indexConversion[sec[1]]
-	for (var i = 0, len = a.length; i < len; i++) { document.querySelector(`.secTwo div:nth-child(${a[i]})`).classList.add('one') }
-}
-
-setInterval(updateDisplay, 1000);
-updateDisplay();
-
-document.querySelector('input').addEventListener('change', function() {
-	document.documentElement.style.setProperty('--oneColors', this.value);
-});
+	const time = getTime(), columns = document.querySelectorAll('.container > div');  
+	let a,index = 0;
+	// Reset Lights //
+	a = document.querySelectorAll('.one');
+	for (let i = 0; i < a.length; i++) a[i].classList.remove('one');
+	// Assign Lights //
+	for (let j = 0, len = columns.length; j < len; j++) {
+		a = b_i[time[columns[j].dataset.tid][index]];
+		for (let k = 0, _len = a.length; k < _len; k++)
+			columns[j].children[a[k]].classList.add('one');
+		index = Number(!index);
+	}
+} setInterval(updateDOM, 1000); updateDOM(); } })()
