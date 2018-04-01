18620e5866f43047e4f876ed07c4205e0342cd30
๖ۜG н σ ѕ т彡 <mig123@hotmail.ca>
2018-04-01 12:46:51 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,14 +2,11 @@
 
 (function () {
 	window.onload = function () {
-		var b_i = [[], [3], [2], [2, 3], [1], [1, 3], [1, 2], [1, 2, 3], [0], [0, 3]];
-		function getTime() {
-			return new Date().toLocaleTimeString('en-CA', { hour12: false }).split(/:| /).map(function (t) {
-				return t.padStart(2, '0');
-			});
-		}
+		var binaryIndexes = [[], [3], [2], [2, 3], [1], [1, 3], [1, 2], [1, 2, 3], [0], [0, 3]];
 		function updateDOM() {
-			var time = getTime(),
+			var time = new Date().toLocaleTimeString().split(/:| /).map(function (t) {
+				return t.padStart(2, '0');
+			}),
 			    columns = document.querySelectorAll('.container > div');
 			// Reset Lights //
 			var a = document.querySelectorAll('.one');
@@ -17,7 +14,7 @@
 				a[i].classList.remove('one');
 			} // Assign Lights //
 			for (var j = 0, _len = columns.length; j < _len; j++) {
-				var b = b_i[time[0 | j / 2][j % 2]];
+				var b = binaryIndexes[time[0 | j / 2][j % 2]];
 				for (var k = 0, __len = b.length; k < __len; k++) {
 					columns[j].children[b[k]].classList.add('one');
 				}
